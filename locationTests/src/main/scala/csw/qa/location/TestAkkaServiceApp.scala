78fc6f2f5f46b3546ad1ff3294d807605487d29f
abrighton <78px3WT5s>
2017-06-13 19:41:58 +0200

    Added extra srg to location tests

--- locationTests/src/main/scala/csw/qa/location/TestAkkaServiceApp.scala
+++ locationTests/src/main/scala/csw/qa/location/TestAkkaServiceApp.scala
@@ -14,6 +14,7 @@ import scala.concurrent.duration._
   * Starts one or more akka services in order to test the location service.
   * If a command line arg is given, it should be the number of services to start (default: 1).
   * An additional command line arg indicates the service number to start with (default: 1)
+  * A third command line arg indicates the number of seconds before unregistering and shutdown (default: 0)
   * Each service will have a number appended to its name.
   * You should start the TestServiceClient with the same number, so that it
   * will try to find all the services.
@@ -28,16 +29,17 @@ object TestAkkaServiceApp extends App {
   // Starts the given number of akka services (or 1)
   val numServices = args.headOption.map(_.toInt).getOrElse(1)
   val firstService = args.tail.headOption.map(_.toInt).getOrElse(1)
+  val autostop = args.tail.tail.headOption.map(_.toInt).getOrElse(0)
   for (i <- firstService until firstService + numServices) {
-    system.actorOf(TestAkkaService.props(i, locationService))
-    Thread.sleep(10) // Avoid timeouts?
+    system.actorOf(TestAkkaService.props(i, autostop, locationService))
+    Thread.sleep(100) // Avoid timeouts?
   }
 
 }
 
 object TestAkkaService {
   // Creates the ith service
-  def props(i: Int, locationService: LocationService): Props = Props(new TestAkkaService(i, locationService))
+  def props(i: Int, autostop: Int, locationService: LocationService): Props = Props(new TestAkkaService(i, autostop, locationService))
 
   // Component ID of the ith service
   def componentId(i: Int) = ComponentId(s"TestAkkaService_$i", ComponentType.Assembly)
@@ -48,17 +50,24 @@ object TestAkkaService {
   // Message sent from client once location has been resolved
   case object ClientMessage
 
+  // Message to unregister and quit
+  case object Quit
+
 }
 
 /**
   * A dummy akka test service that registers with the location service
   */
-class TestAkkaService(i: Int, locationService: LocationService) extends Actor with GenericLogger.Simple {
+class TestAkkaService(i: Int, autostop: Int, locationService: LocationService) extends Actor with GenericLogger.Simple {
+  import context.dispatcher
 
   // Register with the location service
   //  Thread.sleep(3000) // Delaying here to test handling of duplicate registrations
   private val reg = Await.result(locationService.register(AkkaRegistration(TestAkkaService.connection(i), self)), 30.seconds)
   log.info(s"Registered service $i as: ${reg.location.connection.name}")
+
+  if (autostop != 0)
+    context.system.scheduler.scheduleOnce(autostop.seconds, self, TestAkkaService.Quit)
 
   override def receive: Receive = {
     // This is the message that TestServiceClient sends when it discovers this service
@@ -68,6 +77,11 @@ class TestAkkaService(i: Int, locationService: LocationService) extends Actor wi
     // This is the message that JTestServiceClient sends when it discovers this service
     case m: JTestAkkaService.ClientMessage =>
       log.info(s"Received java client message from: ${sender()}")
+
+    case TestAkkaService.Quit =>
+      log.info(s"Actor $i is shutting down after $autostop seconds")
+      Await.result(reg.unregister(), 10.seconds)
+      context.stop(self)
 
     case x =>
       log.error(s"Received unexpected message $x")
