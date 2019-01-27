394bd1604d68e1679bd26a8afa3b3eeb70266a1a
abrighton <78px3WT5s>
2019-01-27 18:21:00 +0100

    Testing

--- project/Settings.scala
+++ project/Settings.scala
@@ -14,7 +14,7 @@ object Settings {
     organizationHomepage := Some(url("http://www.tmt.org")),
     version := Version,
     scalaVersion := ScalaVersion,
-    crossPaths := true,
+//    crossPaths := true,
     parallelExecution in Test := false,
     fork := true,
 //    resolvers += "twtmt-maven" at "http://dl.bintray.com/twtmt/maven/",
@@ -24,10 +24,10 @@ object Settings {
 
   lazy val defaultSettings = buildSettings ++ Seq(
     // compile options ScalaUnidoc, unidoc
-    scalacOptions ++= Seq("-target:jvm-1.8", "-encoding", "UTF-8", "-feature", "-deprecation", "-unchecked"),
-    javacOptions in Compile ++= Seq("-source", "1.8"),
-    javacOptions in (Compile, compile) ++= Seq("-source", "1.8", "-target", "1.8", "-Xlint:unchecked", "-Xlint:deprecation"),
-    javaOptions in (Test, run) ++= Seq("-Djava.net.preferIPv4Stack=true")  // For location service use
+//    scalacOptions ++= Seq("-target:jvm-1.8", "-encoding", "UTF-8", "-feature", "-deprecation", "-unchecked"),
+//    javacOptions in Compile ++= Seq("-source", "1.8"),
+//    javacOptions in (Compile, compile) ++= Seq("-source", "1.8", "-target", "1.8", "-Xlint:unchecked", "-Xlint:deprecation"),
+//    javaOptions in (Test, run) ++= Seq("-Djava.net.preferIPv4Stack=true")  // For location service use
   )
 
   lazy val appSettings = defaultSettings ++ Seq(
