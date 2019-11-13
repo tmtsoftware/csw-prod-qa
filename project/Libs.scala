826456004da05bcaa432935171f04f617faa19e2
abrighton <78px3WT5s>
2019-11-13 12:45:14 +0100

    Updated for csw-1.0.0

--- project/Libs.scala
+++ project/Libs.scala
@@ -10,7 +10,7 @@ object Libs {
 
 object CSW {
   private val Org = "com.github.tmtsoftware.csw"
-  private val Version = "1.0.0-RC4"
+  private val Version = "1.0.0"
 
   val `csw-logging-client` = Org %% "csw-logging-client" % Version
   val `csw-commons` = Org %% "csw-commons" % Version
