4bcba85b5304a3ec2f48e47381b530505a2e3672
Allan Brighton <Allan.Brighton@t-online.de>
2021-10-08 14:13:10 +0200

    In progress akka stream socket example, started on test

--- socketServer/src/test/scala/streams/SocketClientStreamTest.scala
+++ socketServer/src/test/scala/streams/SocketClientStreamTest.scala
@@ -21,14 +21,19 @@ class SocketClientStreamTest extends AnyFunSuite {
 
   test("Basic test") {
 
-    // TODO: Add host, port options
     val client1 = new SocketClientStream()
     val client2 = new SocketClientStream()
     val client3 = new SocketClientStream()
 
-    val f1 = client1.send(1, "DELAY 2000").map(s => println(s"resp1: $s"))
-    val f2 = client2.send(2, "DELAY 1000").map(s => println(s"resp2: $s"))
-    val f3 = client3.send(3, "DELAY 500").map(s => println(s"resp3: $s"))
+    def showResult(id: Int, s: String): String = {
+      val result = s"$id: $s"
+      println(result)
+      result
+    }
+
+    val f1 = client1.send(1, "DELAY 2000").map(showResult(1, _))
+    val f2 = client2.send(2, "DELAY 1000").map(showResult(2, _))
+    val f3 = client3.send(3, "DELAY 500").map(showResult(3, _))
 
     val f = for {
       resp1 <- f1
@@ -36,8 +41,9 @@ class SocketClientStreamTest extends AnyFunSuite {
       resp3 <- f3
     } yield {
       client1.terminate()
+      List(resp1, resp2, resp3)
     }
-    Await.ready(f, 3.seconds)
-    System.exit(0)
+    val list = Await.result(f, 3.seconds)
+    assert(list == List("1: COMPLETED", "2: COMPLETED", "3: COMPLETED"))
   }
 }
